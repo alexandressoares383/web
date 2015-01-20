855f8a1d9ccccb08c8d2288a71e7ffc9e53bb01e
asoares383 <alexandre.soares@enken.com.br>
2015-01-20 17:35:47 -0200

    api mean stack

--- sample/routes/index.js
+++ sample/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mean Stack' });
 });
 
 module.exports = router;
