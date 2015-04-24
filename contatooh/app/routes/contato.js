87b53c7e178fb21acc0a657391cdcfeed614eb81
alexandre de souza <alexandresoares383@gmail.com>
2015-04-24 01:11:13 -0300

    angular js

--- contatooh/app/routes/contato.js
+++
@@ -1,6 +0,0 @@
-var controller = require('./app/controller/contato');
-
-module.exports = function(app) {
-	var controller = app.controllers.contato;
-	app.get('/contatos', controller.listaContatos);
-};
