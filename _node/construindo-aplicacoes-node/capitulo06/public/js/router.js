8aaa7c796b1c53504426f346735fecc814f99ad0
asoares383 <alexandre.soares@enken.com.br>
2015-05-03 19:28:50 -0300

    capitulo 6 - autenticacao

---
+++ _node/construindo-aplicacoes-node/capitulo06/public/js/router.js
@@ -0,0 +1,37 @@
+define(['views/index', 'views/register', 'views/login', 'views/forgotPassword'],
+	function(IndexView, RegisterView, LoginView, ForgotPassword){
+		var SocialRouter = Backbone.Router.extend({
+			currentView: null,
+
+			routes: {
+				"index": "index",
+				"login": "login",
+				"register": "register",
+				"forgotPassword": "forgotPassword",
+			},
+
+			changeView: function(view){
+				if( null != this.currentView ) {
+					this.currentView.undelegateEvents();
+				}
+				this.currentView = view;
+				this.currentView.render();
+			},
+
+			index: function(){
+				this.changeView(new IndexView());
+			},
+			login: function(){
+				this.changeView(new LoginView());
+			},
+			forgotPassword: function(){
+				this.changeView(new ForgotPasswordView());
+			},
+			register: function(){
+				this.changeView(new RegisterView());
+			}
+		});
+
+		return new SocialRouter();
+	}
+);
